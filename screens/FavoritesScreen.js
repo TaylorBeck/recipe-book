4b31d6e80bc14b0345d8747da9cceb41d5f8043a
Taylor Beck <beck.taylorg@gmail.com>
2020-04-07 15:16:29 -0700

    Add bottom tab bar navigator

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const FavoritesScreen = props => {
   return (
-    <View>
+    <View style={StyleSheet.screen}>
       <Text>The Favorites Screen</Text>
     </View>
   );
