aba3b3cd2a9abdf9bbfa178966b84e4142c7e859
Taylor Beck <beck.taylorg@gmail.com>
2020-04-09 22:36:19 -0700

    Update filter saving

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen = props => {
   // Category id is passed through navigation params
@@ -14,6 +16,10 @@ const CategoryMealsScreen = props => {
   const displayedMeals = availableMeals.filter(
     meal => meal.categoryIds.indexOf(categoryId) >= 0 // Use indexOf to check existence of categoryId in array
   );
+
+  if (displayedMeals.length === 0) {
+    return <View style={styles.content}><DefaultText>No recipes. Please check your filters...</DefaultText></View>
+  }
 
   return (
     <MealList listData={displayedMeals} navigation={props.navigation} />
@@ -28,5 +34,13 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default CategoryMealsScreen;
